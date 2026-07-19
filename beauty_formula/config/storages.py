d00d243d294228d701f766635e5babe3c24e40ca
bragga-dev <bragawebdevelopment@gmail.com>
2026-07-18 21:45:12 -0300

    refector(auth):Correção das configurações de permissão do minio

--- beauty_formula/config/storages.py
+++ beauty_formula/config/storages.py
@@ -4,21 +4,21 @@ import os
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaFilesStorage(S3Boto3Storage):
-    bucket_name = os.environ.get("MINIO_BUCKET_MEDIA", "beauty_formula-media")
+    bucket_name = os.environ.get("MINIO_BUCKET_MEDIA", "beautyformulamedia")
     default_acl = "public-read"
     file_overwrite = False
     custom_domain = (
     f"{os.environ.get('MINIO_PUBLIC_URL')}/"
-    f"{os.environ.get('MINIO_BUCKET_MEDIA', 'beauty_formula-media')}")
+    f"{os.environ.get('MINIO_BUCKET_MEDIA', 'beautyformulamedia')}")
     url_protocol = os.environ.get("MINIO_URL_PROTOCOL", "http:")
 
 class StaticFilesStorage(S3Boto3Storage):
-    bucket_name = os.environ.get("MINIO_BUCKET_STATIC", "beauty_formula-static")
+    bucket_name = os.environ.get("MINIO_BUCKET_STATIC", "beautyformulastatic")
     default_acl = "public-read"
     file_overwrite = True
 
 class PrivateFilesStorage(S3Boto3Storage):
-    bucket_name = os.environ.get("MINIO_BUCKET_PRIVATE", "beauty_formula-private")
+    bucket_name = os.environ.get("MINIO_BUCKET_PRIVATE", "beautyformulaprivate")
     default_acl = None        
     file_overwrite = False
     querystring_auth = True   
