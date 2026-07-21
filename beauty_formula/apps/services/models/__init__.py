16c398a3b9aca9f06054d6971d926d995fcb9041
bragga-dev <bragawebdevelopment@gmail.com>
2026-07-21 14:17:21 -0300

    refector(auth):Correção do recurso para detalhes de Funcionário

--- beauty_formula/apps/services/models/__init__.py
+++ beauty_formula/apps/services/models/__init__.py
@@ -2,9 +2,8 @@ from beauty_formula.apps.services.models.service import Service
 from beauty_formula.apps.services.models.average_rating import AverageRating
 from beauty_formula.apps.services.models.employee_time_off import EmployeeTimeOff
 from beauty_formula.apps.services.models.employee_works_hours import EmployeeWorkingHours
+from beauty_formula.apps.services.models.employee_service import EmployeeService
 from beauty_formula.apps.services.models.scheduling import Scheduling
-
-
 
 
 __all__ = [
@@ -12,5 +11,6 @@ __all__ = [
     'AverageRating',
     'EmployeeTimeOff',
     'EmployeeWorkingHours',
+    'EmployeeService',
     'Scheduling',
 ]
